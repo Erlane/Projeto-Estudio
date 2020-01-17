c647225ee79b48074c386f37073671f9a045d032
João Paulo <joaop.rosa01@gmail.com>
2020-01-17 12:26:56 -0300

    meu projeto

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
             <img className='filmes-images' src={Fantastico} /> 
             <p className='filmes-assistir'>quero ver</p>
             <h2>Capitão Fantástico </h2> 
+            
             <p>Nas florestas do estado de Washington, um pai cria seus seis filhos longe da civilização,    em uma rígida rotina de aventuras. Ele é forçado a deixar o isolamento e leva sua família    para encarar o mundo, desafiando sua ideia do que significa ser pai.</p>
           </div> 
           <div className='filmes_itens'>
