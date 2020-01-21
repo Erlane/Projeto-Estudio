9fee02ff5f6f5a1842a07de776398c5b9b9088b4
João Paulo <joaop.rosa01@gmail.com>
2020-01-20 22:47:46 -0300

    finish project

--- src/App.js
+++ src/App.js
@@ -1,45 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import Fant from './assets/fant.jpeg';
+import React, {Component} from 'react';
+import Fant from './assets/capitao-fantastico.jpg';
 import Fantastico from './assets/fantastico.jpeg';
 import Corra from './assets/corra.jpeg'
+import Bacurau from './assets/bacurau.jpeg'
+import Modal from './components/Modal';
 
 //Components
-import Home from './components/Home';
 import './App.css';
 
-function App() {
-  return ( 
-    <div> 
-      {/* <header className="App-header"> 
-      <img src={logo} className="App-logo" alt="logo" /> 
-      <p> 
-        Edit <code>src/App.js</code> and save to reload. 
-      </p>
-      </header>*/} 
-      <Home/>
-        <img className='image' src={Fant} />
-        <p className='paragrafy-lista'>Minha lista: </p> 
-        <div className='filmes'> 
-          <div className='filmes_itens'> 
-            <p>5.0</p>
-            <img className='filmes-images' src={Fantastico} /> 
-            <p className='filmes-assistir'>quero ver</p>
-            <h2>Capitão Fantástico </h2> 
-            
-            <p>Nas florestas do estado de Washington, um pai cria seus seis filhos longe da civilização,    em uma rígida rotina de aventuras. Ele é forçado a deixar o isolamento e leva sua família    para encarar o mundo, desafiando sua ideia do que significa ser pai.</p>
-          </div> 
-          <div className='filmes_itens'>
-            <p>2.0</p>
-            <img className='filmes-images' src={Corra} /> 
-            <p className='filmes-assistir'>quero ver</p>
-            <h2>Corra!</h2> 
-            <p>Um jovem fotógrafo descobre um segredo sombrio quando conhece os pais aparentemente          amigáveis da sua namorada.</p> 
-        </div> 
-      </div> 
-    </div> 
+class App extends Component {
+  state = {
+    isModal: undefined,
+    isModalCategory: undefined,
+  };
+    
+  isOpen = (event) => {
+    event.stopPropagation();
+    this.setState({
+      isModal: true,
+    })
+  }
 
-  );
+  isClose = (event) => {
+    event.stopPropagation();
+    this.setState({
+       isModal:false,
+    })
+  }
+
+  handleIsModal = (event) => {
+    event.stopPropagation();
+    this.setState({
+      isModal: !this.state.isModal,
+    })
+  }
+
+  renderModalCategory = () => {
+    return (
+      this.state.isModalCategory && (
+        <span className='modal-category'>
+          <p>Quero ver</p>
+          <p>Já vistos</p>
+        </span>
+        )
+    )
+  }
+
+  render() {
+    return ( 
+      <div> 
+        <div>
+          {this.state.isModal && <Modal handleCloseModal={this.isClose}/> }
+          <div className='menu'>
+            <h1>ToDoFlix</h1>
+            <div className='buttons'> 
+              <button className='button-category' onClick={this.handleIsModalCategory}>
+                categorias
+              </button>
+              {this.renderModalCategory}
+              <button className='button-add' onClick={this.isOpen}>adicionar filme</button>
+              <button className='button-add-mobile' onClick={this.isOpen}>add filme</button>
+            </div> 
+          </div>
+          <hr/>
+          <div className='cartaz'>
+            <img className='cartaz-image' src={Fant} />
+          </div>
+          <p className='paragrafy-lista'>Minha lista: </p> 
+          <div className='film'> 
+            <div className='film-itens'> 
+              <img className='film-images' src={Fantastico} /> 
+              <button className='film-assistir'>quero ver</button>
+              <h2>Capitão Fantástico</h2> 
+              <p>Nas florestas do estado de Washington, um pai cria seus seis filhos longe da civilização,    em uma rígida rotina de aventuras. Ele é forçado a deixar o isolamento e leva sua família      para encarar o mundo, desafiando sua ideia do que significa ser pai.</p>
+            </div> 
+            <div className='film-itens'>
+              <img className='film-images' src={Corra} /> 
+              <button className='film-assistir'>quero ver</button>
+              <h2>Corra!</h2> 
+              <p>Um jovem fotógrafo descobre um segredo sombrio quando conhece os pais aparentemente      s amigáveis da sua namorada.
+              </p> 
+            </div> 
+            <div className='film-itens'>
+              <img className='film-images' src={Bacurau} />
+              <button className='film-assistir1'>já vi</button>
+              <h2>Bacurau</h2>
+              <p> Os moradores de um pequeno povoado do sertão brasileiro, chamado Bacurau, descobrem que a   comunidade não consta mais em qualquer mapa.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,65 +1,128 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../App.css';
-// import ImageHome from '../assets/images.jpeg';
-// import ImageForm from '../assets/claquete.svg';
 
-class Home extends React.Component { 
-    // formulario = () => (
- 
-    // ) 
-    render (){ 
-        return (
-            <div>
-            <figure>
-                {/* <img src={ImageForm}></img> */} 
-            </figure> 
-            <form className='form'> 
-                <label> 
-                    Nome: 
-                    <input 
-                    type='text' 
-                    // onChange={this.nome} 
-                    // value={this.state.nome}
-                    /> 
-                </label> 
-                <label>
-                    Descrição:
-                    <input 
-                    type='text'
-                    // onChange={this.descricao}
-                    // value={this.state.descricao} 
-                    /> 
-                </label>
-                <label> 
-                    Status:
-                    <input 
-                    type='text'
-                    // onChange={this.Status} 
-                    // value={this.state.Status} 
-                    /> 
-                </label>
-                <label> 
-                    Imagem de exibição:
-                    <input 
-                    type='text' 
-                    // onChange={this.imagemExibicao}
-                    // value={this.state.imagemExibicao} 
-                    /> 
-                </label> 
-                <button>adicionar imagem</button> 
-                <label>
-                    Nota: 
-                    <input
-                    type='text'
-                    // onChange={this.Nota} 
-                    // value={this.state.Nota} 
-                    /> 
-                </label> 
-             </form> 
-             {/* <img src={ImageHome}></img> */} 
-            </div> 
-        )                      
+//Images
+import ImageForm from '../assets/claquete.svg';
+import Estrelas from '../assets/estrela-cinza.svg';
+
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        nome: null,
+        descricao: null,
+        status: null,
     }
+  }
+
+  changeNome = (ev) => {
+    this.setState({
+        nome: ev.target.value,
+    });
+  }
+
+  changeDescricao = (ev) => {
+    this.setState({
+        descricao: ev.target.value,
+    });
+  }
+
+  changeStatus = (ev) => {
+    this.setState({
+        status: ev.target.value,
+    });
+  }
+
+  changeImagemExibicao = (ev) => {
+    this.setState({
+        imagemExibicao: ev.target.value,
+    });
+  }
+
+  changeNota = (ev) => {
+    this.setState({
+        nota: ev.target.value,
+    });
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  render (){
+    return (
+      <div className='modal'>
+        <div className='modal-container'>
+          <figure>
+            <img src={ImageForm}></img> 
+          </figure> 
+          <div className='modal-form'>
+            <div className='modal-inicio'>
+              <h3 className='modal-title'>Adicionar novo filme:</h3>
+              <p className="sair" onClick={this.props.handleCloseModal}>X</p>
+            </div>
+            <form className='form' onSubmit={this.handleSubmit}> 
+              <label> 
+                Nome: 
+              </label> 
+              <input 
+                tgoriype='text' 
+                autoFocus
+                onChange={this.changeNome}
+                value={this.state.nome}
+              /> 
+              <label>
+                Descrição:
+              </label>
+                <input 
+                  type='text'
+                  onChange={this.changeDescricao}
+                  value={this.state.descricao} 
+                /> 
+              <label> 
+                Status:
+              </label>
+              <input 
+                type='text'
+                onChange={this.changeStatus} 
+                value={this.state.status} 
+                /> 
+              <label> 
+                Imagem de exibição:
+              </label> 
+              <spam className='spam-exibicao'> 
+                <input  className='ImagemExibicao'
+                  // type='url' 
+                  type="image"
+                  onChange={this.changeImagemExibicao}
+                  value={this.state.imagemExibicao} 
+                /> 
+                <button className='modal-button'>adicionar imagem</button>  
+              </spam>
+              <label>
+                Nota: 
+              </label> 
+              <spam className='spam-estrela'>
+                <img className='estrela' src={Estrelas}></img> 
+                <img className='estrela' src={Estrelas}></img> 
+                <img className='estrela' src={Estrelas}></img> 
+                <img className='estrela' src={Estrelas}></img> 
+                <img className='estrela' src={Estrelas}></img> 
+              </spam>
+              <spam className='modal-spam'>
+                <spam className='modal-cancelar' onClick={this.props.handleCloseModal}>cancelar</spam>
+                <button className='modal-feito' type='submit' value="Enviar">feito</button>
+              </spam>
+            </form> 
+          </div>
+        </div>
+      </div> 
+    )                      
+  }
 }
 
-export default Home;
+export default Modal;
